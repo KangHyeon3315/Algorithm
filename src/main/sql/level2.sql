ee14efc8183691bee97064b1adbf8b62b7873086
KangHyeon3315 <son2786w@gmail.com>
2023-07-26 13:09:57 +0900

    알고리즘 공부 14일차

--- src/main/sql/level2.sql
+++ src/main/sql/level2.sql
@@ -61,3 +61,52 @@ SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS
 -- SUBSTR 함수를 사용하면 문자열의 일부를 추출할 수 있다. (첫 문자 Index는 1부터 시작)
 -- SUBSTR(문자열, 시작 Index, 길이)
 SELECT SUBSTR(PRODUCT_CODE, 1, 2) AS CATEGORY, COUNT(*) AS PRODUCTS FROM PRODUCT GROUP BY CATEGORY ORDER BY CATEGORY
+
+-- 가격대 별 상품 개수 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131530
+SELECT (PRICE - PRICE % 10000) AS PRICE_GROUP, COUNT(*) AS PRODUCTS FROM PRODUCT GROUP BY PRICE_GROUP ORDER BY PRICE_GROUP;
+
+-- 상품 별 오프라인 매출 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131533
+SELECT PRODUCT_CODE, P.PRICE * SUM(SALES_AMOUNT) AS SALES FROM PRODUCT AS P JOIN OFFLINE_SALE AS O ON P.PRODUCT_ID = O.PRODUCT_ID GROUP BY P.PRODUCT_ID ORDER BY SALES DESC, PRODUCT_CODE;
+
+-- 재구매가 일어난 상품과 회원 리스트 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131536
+SELECT USER_ID, PRODUCT_ID FROM ONLINE_SALE GROUP BY USER_ID, PRODUCT_ID HAVING COUNT(*) >= 2 ORDER BY USER_ID, PRODUCT_ID DESC
+
+-- 진료과별 총 예약 횟수 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132202
+SELECT MCDP_CD AS 진료과코드, COUNT(*) AS 5월예약건수 FROM APPOINTMENT WHERE YEAR(APNT_YMD) = '2022' AND MONTH(APNT_YMD) = '5' GROUP BY MCDP_CD ORDER BY 5월예약건수, 진료과코드;
+
+-- 성분으로 구분한 아이스크림 총 주문량
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133026
+SELECT INGREDIENT_TYPE, SUM(TOTAL_ORDER) AS TOTAL_ORDER FROM FIRST_HALF AS F JOIN ICECREAM_INFO AS I ON F.FLAVOR = I.FLAVOR GROUP BY INGREDIENT_TYPE;
+
+-- 조건에 맞는 도서와 저자 리스트 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144854
+SELECT BOOK_ID, AUTHOR_NAME, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE FROM BOOK AS B JOIN AUTHOR AS A ON B.AUTHOR_ID = A.AUTHOR_ID WHERE CATEGORY='경제' ORDER BY PUBLISHED_DATE
+
+-- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/151137
+SELECT CAR_TYPE, COUNT(*) AS CARS FROM CAR_RENTAL_COMPANY_CAR WHERE OPTIONS LIKE '%통풍시트%' OR OPTIONS LIKE '%열선시트%' OR OPTIONS LIKE '%가죽시트%' GROUP BY CAR_TYPE ORDER BY CAR_TYPE
+
+-- 자동차 평균 대여 기간 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157342
+SELECT CAR_ID, ROUND(AVG(TIMESTAMPDIFF(DAY, START_DATE, END_DATE) + 1), 1) AS AVERAGE_DURATION FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY GROUP BY CAR_ID HAVING AVERAGE_DURATION >= 7 ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
+
+-- 조건에 부합하는 중고거래 상태 조회하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164672
+-- CASE WHEN 조건 THEN 값1 WHEN 조건 THEN 값2 ELSE 값3 END
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN (STATUS='SALE') THEN '판매중'
+        WHEN (STATUS='RESERVED') THEN '예약중'
+        WHEN (STATUS='DONE') THEN '거래완료'
+        END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC
